4dffd5b0923c6d3c04c9536dda70d03c416ebb62
Aryan Kaushik <aryankaushik2023@gmail.com>
2024-09-26 17:13:10 +0530

    initialise: add the tool, sample file and readme

---
+++ ga.py
@@ -0,0 +1,13 @@
+  GNU nano 4.8                                                                                       ga.py                                                                                                 
+import subprocess
+import csv
+
+user_emails_string = subprocess.check_output(["zmprov", "-l", "gaa"], text=True)
+user_emails_list = user_emails_string.strip().split("\n")
+with open('output_columns.csv', mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Email", "Name"])
+        for user_email in user_emails_list:
+                name = subprocess.check_output(["zmprov", "ga", user_email, "|", "grep", "sn"], text=True)
+                name = name.split("\n")[1][4::]
+                writer.writerow([user_email, name])
