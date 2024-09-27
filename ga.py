ae0210eb684c1c18f7e4188c133129ad663ef54b
Aryan Kaushik <34372791+Aryan20@users.noreply.github.com>
2024-09-27 12:42:37 +0530

    tool: add backwards compatibility with Python 3.6 and less

--- ga.py
+++ ga.py
@@ -1,13 +1,12 @@
-  GNU nano 4.8                                                                                       ga.py                                                                                                 
 import subprocess
 import csv
 
-user_emails_string = subprocess.check_output(["zmprov", "-l", "gaa"], text=True)
+user_emails_string = subprocess.check_output(["zmprov", "-l", "gaa"], universal_newlines=True)
 user_emails_list = user_emails_string.strip().split("\n")
 with open('output_columns.csv', mode='w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(["Email", "Name"])
         for user_email in user_emails_list:
-                name = subprocess.check_output(["zmprov", "ga", user_email, "|", "grep", "sn"], text=True)
+                name = subprocess.check_output(["zmprov", "ga", user_email, "|", "grep", "sn"], universal_newlines=True)
                 name = name.split("\n")[1][4::]
                 writer.writerow([user_email, name])
